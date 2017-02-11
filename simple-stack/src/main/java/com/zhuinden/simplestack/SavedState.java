910e748c3555e4535e6af5eb57b26960bfbd0bdd
zhuinden <zhuinden@gmail.com>
2017-02-11 14:23:16 +0100

    missing @Nullable

--- simple-stack/src/main/java/com/zhuinden/simplestack/SavedState.java
+++ simple-stack/src/main/java/com/zhuinden/simplestack/SavedState.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.SparseArray;
 
 /**
@@ -84,7 +85,7 @@ public class SavedState
             return this;
         }
 
-        public Builder setBundle(Bundle bundle) {
+        public Builder setBundle(@Nullable Bundle bundle) {
             this.bundle = bundle;
             return this;
         }
