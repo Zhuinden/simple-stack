78c8f9f53382f060835480f2db594974cdab6875
zhuinden <zhuinden@gmail.com>
2017-02-23 20:23:36 +0100

    added test to verify back destroy/create

--- simple-stack/src/test/java/com/zhuinden/simplestack/NestedStackTest.java
+++ simple-stack/src/test/java/com/zhuinden/simplestack/NestedStackTest.java
@@ -1,5 +1,7 @@
 package com.zhuinden.simplestack;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -102,5 +104,45 @@ public class NestedStackTest {
         assertThat(nestedStack2.getHistory()).isEmpty();
         assertThat(nestedStack.getHistory()).containsExactly(rootKeyA1);
         assertThat(backstackDelegate.getBackstack().getHistory()).containsExactly(rootKey);
+    }
+
+    @Test
+    public void nestedStackBackDestroysUnneededServicesAndCreatesPrevious() {
+        TestKey rootKey = new TestKey("root");
+        TestKey rootKeyA1 = new TestKey("rootA1");
+        TestKey rootKeyA2 = new TestKey("rootA2");
+        TestKey rootKeyB1 = new TestKey("rootB1");
+
+        BackstackDelegate backstackDelegate = BackstackDelegate.create();
+        backstackDelegate.onCreate(null, null, HistoryBuilder.single(rootKey));
+        backstackDelegate.setStateChanger(stateChanger);
+        completionCallback.stateChangeComplete();
+
+        NestedStack nestedStack = backstackDelegate.findService(rootKey, BackstackManager.LOCAL_STACK);
+        nestedStack.initialize(rootKeyA1, rootKeyA2);
+        nestedStack.setStateChanger(stateChanger);
+        completionCallback.stateChangeComplete();
+
+        try {
+            nestedStack.findService(rootKeyA1, BackstackManager.LOCAL_STACK);
+            Assert.fail();
+        } catch(IllegalStateException e) {
+            // OK!
+        }
+        NestedStack nestedStack2 = nestedStack.findService(rootKeyA2, BackstackManager.LOCAL_STACK);
+        nestedStack2.initialize(rootKeyB1);
+        nestedStack2.setStateChanger(stateChanger);
+        completionCallback.stateChangeComplete();
+
+        nestedStack2.goBack();
+        completionCallback.stateChangeComplete();
+
+        try {
+            nestedStack.findService(rootKeyA2, BackstackManager.LOCAL_STACK);
+            Assert.fail();
+        } catch(IllegalStateException e) {
+            // OK!
+        }
+        assertThat(nestedStack.findService(rootKeyA1, BackstackManager.LOCAL_STACK)).isNotNull();
     }
 }
