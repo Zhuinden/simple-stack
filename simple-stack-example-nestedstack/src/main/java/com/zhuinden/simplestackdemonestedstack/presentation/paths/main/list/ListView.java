44764052705b5faf81ad3dd9980f764eb4e9c05b
Zhuinden <zhuinden@gmail.com>
2017-02-27 00:46:05 +0100

    get keys

--- simple-stack-example-nestedstack/src/main/java/com/zhuinden/simplestackdemonestedstack/presentation/paths/main/list/ListView.java
+++ simple-stack-example-nestedstack/src/main/java/com/zhuinden/simplestackdemonestedstack/presentation/paths/main/list/ListView.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
+import com.zhuinden.simplestack.Backstack;
+
 /**
  * Created by Zhuinden on 2017.02.19..
  */
@@ -13,18 +15,30 @@ public class ListView
         extends RelativeLayout {
     public ListView(Context context) {
         super(context);
+        init(context);
     }
 
     public ListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public ListView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     @TargetApi(21)
     public ListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    ListKey listKey;
+
+    private void init(Context context) {
+        if(!isInEditMode()) {
+            listKey = Backstack.getKey(context);
+        }
     }
 }
