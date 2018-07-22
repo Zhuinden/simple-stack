c3d90e86364f7df63efbd86d5740013fdca82b94
Zhuinden <zhuinden@gmail.com>
2018-07-22 02:13:07 +0200

    Remove `flow-sample`, `flow-master-detail-sample`, `mortar-sample`.
    The future leads us towards replacing Master-Detail alignments with
    ConstraintLayout's ConstraintLayoutStates, see Chrome OS Resizing.
    
    The `Flow-Alpha-Master-Detail` was much less hacky anyways.

--- simple-stack-flow-masterdetail-fragments/src/main/java/com/example/stackmasterdetailfrag/application/IsMasterView.java
+++
@@ -1,7 +0,0 @@
-package com.example.stackmasterdetailfrag.application;
-
-import com.example.stackmasterdetailfrag.paths.MasterDetailPath;
-
-public interface IsMasterView {
-    void updateSelection(MasterDetailPath newPath);
-}
--- simple-stack-flow-masterdetail/src/main/java/com/example/stackmasterdetail/application/IsMasterView.java
+++
@@ -1,7 +0,0 @@
-package com.example.stackmasterdetail.application;
-
-import com.example.stackmasterdetail.paths.MasterDetailPath;
-
-public interface IsMasterView {
-    void updateSelection(MasterDetailPath newPath);
-}
