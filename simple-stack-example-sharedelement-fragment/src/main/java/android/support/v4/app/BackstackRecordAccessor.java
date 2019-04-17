a94b268eeaefe630ba35dc1fe60e6f6a8e7eca03
zhuinden <zhuinden@gmail.com>
2019-04-18 01:36:32 +0200

    Add some comments for the accessor

--- simple-stack-example-sharedelement-fragment/src/main/java/android/support/v4/app/BackstackRecordAccessor.java
+++ simple-stack-example-sharedelement-fragment/src/main/java/android/support/v4/app/BackstackRecordAccessor.java
@@ -2,10 +2,23 @@ package android.support.v4.app;
 
 import java.util.ArrayList;
 
+/**
+ * This class allows writing the source view's transition name, without having to pass the View itself to the FragmentManager.
+ *
+ * @author zhuinden
+ */
 public class BackstackRecordAccessor {
     private BackstackRecordAccessor() {
     }
 
+    /**
+     * To skip passing the View itself (which would be stored as transient WeakReference), we can instead write the source name into the FragmentTransaction directly.
+     *
+     * @param fragmentTransaction  the target fragment transaction
+     * @param sourceTransitionName the source transition name
+     * @param targetTransitionName the target transition name
+     * @return the fragment transaction
+     */
     public static FragmentTransaction addSharedElement(FragmentTransaction fragmentTransaction, String sourceTransitionName, String targetTransitionName) {
         if(FragmentTransition.supportsTransition() && fragmentTransaction instanceof BackStackRecord) {
             if(sourceTransitionName == null) {
