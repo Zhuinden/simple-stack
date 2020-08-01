67d467fecdfc53b0f1bdaf18a17b37dcbd39a43b
zhuinden <zhuinden@gmail.com>
2020-08-01 14:45:43 +0200

    Update gradle and build tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.3")
+        classpath("com.android.tools.build:gradle:4.0.1")
         classpath("me.tatarka:gradle-retrolambda:3.7.0")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-        classpath("io.realm:realm-gradle-plugin:7.0.0")
+        classpath("io.realm:realm-gradle-plugin:7.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
