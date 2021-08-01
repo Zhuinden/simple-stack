c1c59efc38e07c954a9398926b4a0592a1cf9f8f
Gabor Varadi <zhuinden@gmail.com>
2021-08-01 19:53:07 +0200

    Remove unused rxbinding dependency from sample

--- samples/scoping-samples/simple-stack-example-scoping-java/build.gradle.kts
+++ samples/scoping-samples/simple-stack-example-scoping-java/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
 
     implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
-    implementation("com.jakewharton.rxbinding2:rxbinding:2.2.0")
     implementation("com.jakewharton.rxrelay2:rxrelay:2.1.1")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
